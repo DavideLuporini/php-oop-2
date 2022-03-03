d13dd020f71e1ffb0c70063184d951a70ed89950
illupovero <davideluporini1996@gmail.com>
2022-03-03 16:00:01 +0100

    added other 2 classes

---
+++ classes/Accessories.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . '/products.php';
+class Accessories extends Products
+{
+    public $size;
+    public $material;
+
+
+    public function __construct($name, $price, $size, $material)
+    {
+        parent::__construct($name, $price);
+        $this->setSize($size);
+
+        $this->setMaterial($material);
+    }
+    // settings
+    public function setSize($size)
+    {
+        $this->size = $size;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+    public function setMaterial($material)
+    {
+        $this->material = $material;
+    }
+}
---
+++ classes/Games.php
@@ -0,0 +1,35 @@
+<?php
+require_once __DIR__ . '/products.php';
+class Games extends Products
+{
+    public $animal;
+    public $type;
+    public $material;
+
+
+    public function __construct($name, $price, $animal, $type, $material)
+    {
+        parent::__construct($name, $price);
+        $this->setAnimal($animal);
+        $this->setType($type);
+        $this->setMaterial($material);
+    }
+    // settings
+    public function setAnimal($animal)
+    {
+        $this->animal = $animal;
+    }
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+    public function setMaterial($material)
+    {
+        $this->material = $material;
+    }
+}
